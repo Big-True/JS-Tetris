1beb31483967ff492b5436a2a1dee4d2bd5a3ae6
Big_True <big_true@qq.com>
2020-04-25 19:49:18 +0800

    latex fix

--- menu.js
+++ menu.js
@@ -1,13 +1,15 @@
 function drawMenu(gl) {
+    var mainMenuPerHeight = windowHeight / 4 * 3 / defaultMenuOptions.length;
+    var mainMenuPerWidth = 3 * mainMenuPerHeight > windowWidth ? windowWidth : 3 * mainMenuPerHeight;
     gl.drawImage(logo, (windowWidth - windowHeight / 24 * 7) / 2, 0, (windowHeight / 24 * 7), (windowHeight / 4));
     gl.strokeStyle = 'rgb(255,255,255)';
     gl.fillStyle = 'rgb(255,255,255)';
     gl.lineWidth = 5;
-    gl.font = 'bold ' + (windowHeight / 4 * 3 / defaultMenuOptions.length - 20) / 2 + 'px Material Icons';
+    gl.font = 'bold ' + mainMenuPerHeight / 2 + 'px Material Icons';
     gl.textAlign = 'center';
     gl.textBaseline = "middle";
     for (var i = 0; i < defaultMenuOptions.length; ++i) {
-        gl.strokeRect(windowWidth / 3, windowHeight / 4 + windowHeight / 4 * 3 / defaultMenuOptions.length * i + 10, windowWidth / 3, windowHeight / 4 * 3 / defaultMenuOptions.length - 20);
-        gl.fillText(defaultMenuOptions[i], windowWidth / 2, windowHeight / 4 + windowHeight / 4 * 3 / defaultMenuOptions.length * (i + 0.5), windowWidth / 3);
+        gl.strokeRect((windowWidth - mainMenuPerWidth) / 2, windowHeight / 4 + mainMenuPerHeight * i + 10, mainMenuPerWidth, mainMenuPerHeight - 20);
+        gl.fillText(defaultMenuOptions[i], windowWidth / 2, windowHeight / 4 + mainMenuPerHeight * (i + 0.5), mainMenuPerWidth);
     }
 }
